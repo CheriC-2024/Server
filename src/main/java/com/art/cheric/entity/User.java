13825455bbbc334ceeab18e5fca4a8ef5f3e2d68
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-05-30 07:57:55 +0900

    ♻️ Refactor: Add user profileImage column (#3)

--- src/main/java/com/art/cheric/entity/User.java
+++ src/main/java/com/art/cheric/entity/User.java
@@ -1,10 +1,12 @@
 package com.art.cheric.entity;
 
 import com.art.cheric.constant.Role;
+import com.art.cheric.dto.UserCreateRequestDto;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.Comment;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -29,8 +31,12 @@ public class User extends BaseTime {
     @Comment("사용자 이름")
     private String name;
 
-    @Column(name = "profileImgPath", length = 500, nullable = false)
+    @Column(name = "profileImg", length = 500, nullable = false)
     @Comment("사용자 이미지")
+    private String profileImg;
+
+    @Column(name = "profileImgPath", length = 500, nullable = false)
+    @Comment("사용자 이미지 경로")
     private String profileImgPath;
 
     @Enumerated(EnumType.STRING)
@@ -40,15 +46,29 @@ public class User extends BaseTime {
 
     // TODO: 체리 구매까지 확장되면 어떻게 할지 더 고민하기
     @Column(name = "cherry", nullable = false)
-    @Comment("체리 개수")
+    @Comment("보유 체리 개수")
+    @ColumnDefault("0")
     private int cherry;
 
     @Builder
-    public User(String email, String name, String profileImgPath, Role role) {
+    public User(String email, String name, String profileImg, String profileImgPath, Role role, int cherry) {
         this.email = email;
         this.name = name;
+        this.profileImg = profileImg;
         this.profileImgPath = profileImgPath;
         this.role = role;
+        this.cherry = cherry;
+    }
+
+    public static User createUser(UserCreateRequestDto userCreateRequestDto) {
+        return User.builder()
+                .name(userCreateRequestDto.getName())
+                .email(userCreateRequestDto.getEmail())
+                .profileImg(userCreateRequestDto.getProfileImg())
+                .profileImgPath(userCreateRequestDto.getProfileImgPath())
+                .role(userCreateRequestDto.getRole())
+                .cherry(userCreateRequestDto.getCherry())
+                .build();
     }
 
     @Override
