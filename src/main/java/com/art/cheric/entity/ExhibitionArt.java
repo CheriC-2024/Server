7b5a5daeb425c68fc5e6caf33470c532b7d9739a
yerim <97941141+yerim123456@users.noreply.github.com>
2024-05-29 19:44:08 +0900

    ✨ feat: Add exhibitionArt entity (#3)

---
+++ src/main/java/com/art/cheric/entity/ExhibitionArt.java
@@ -0,0 +1,48 @@
+package com.art.cheric.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Objects;
+
+
+@Entity
+@EntityListeners(value = {AuditingEntityListener.class})
+@Table(name = "ExhibitionArt")
+@NoArgsConstructor
+@Getter
+public class ExhibitionArt  extends BaseTime {
+    @Id
+    @Column(name = "exhibition_art_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "art_id", nullable = false)
+    @Comment("컬렉션 작품")
+    private Art art;
+
+    @Builder
+    public ExhibitionArt(Art art) {
+        this.art = art;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof ExhibitionArt exhibitionArt)) return false;
+
+        return Objects.equals(this.id, exhibitionArt.getId()) &&
+                Objects.equals(this.art, exhibitionArt.getArt());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, art);
+    }
+
+}
