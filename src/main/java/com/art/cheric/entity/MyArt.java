9fd77198e24a90728d711c6ab482d2bac0955ed5
yerim <97941141+yerim123456@users.noreply.github.com>
2024-05-29 19:44:32 +0900

    ✨ feat: Add myArt entity (#3)

---
+++ src/main/java/com/art/cheric/entity/MyArt.java
@@ -0,0 +1,53 @@
+package com.art.cheric.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Objects;
+
+@Entity
+@EntityListeners(value = {AuditingEntityListener.class})
+@Table(name = "MyArt")
+@NoArgsConstructor
+@Getter
+public class MyArt extends BaseTime {
+    @Id
+    @Column(name = "my_art_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "art_id", nullable = false)
+    @Comment("소유 작품")
+    private Art art;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @Comment("작품 소유자")
+    private User user;
+
+    @Builder
+    public MyArt(Art art, User user) {
+        this.art = art;
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof MyArt myArt)) return false;
+
+        return Objects.equals(this.user, myArt.getUser()) &&
+                Objects.equals(this.art, myArt.getArt());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, art);
+    }
+
+}
