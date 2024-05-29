f8936d650bd6bb1ac29255b52787f027c8572b00
yerim <97941141+yerim123456@users.noreply.github.com>
2024-05-29 14:29:37 +0900

    ✨ feat: Add initial setting(#1)

---
+++ src/main/java/com/art/cheric/ChericApplication.java
@@ -0,0 +1,13 @@
+package com.art.cheric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChericApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChericApplication.class, args);
+	}
+
+}
