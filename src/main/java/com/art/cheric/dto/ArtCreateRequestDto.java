969d610b87ee88da78f2e60325c41e71dfe0a0a7
yerim <97941141+yerim123456@users.noreply.github.com>
2024-06-05 11:17:03 +0900

    🔨 Fix : Modify dto mapping way (#3)

--- src/main/java/com/art/cheric/dto/ArtCreateRequestDto.java
+++ src/main/java/com/art/cheric/dto/ArtCreateRequestDto.java
@@ -3,12 +3,13 @@ package com.art.cheric.dto;
 import com.art.cheric.constant.ArtStatus;
 import com.art.cheric.constant.MaterialSort;
 import com.art.cheric.entity.Art;
+import com.art.cheric.entity.Image;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
-@NoArgsConstructor
 public class ArtCreateRequestDto {
     private String name;
     private String series;
@@ -19,14 +20,54 @@ public class ArtCreateRequestDto {
     private MaterialSort materialStatus;
     private String notice;
     private int cherryNum;
+    private List<ImageCreateRequestDto> imageList;
 
-    private static ModelMapper modelMapper = new ModelMapper();
+    // TODO: 추후 modelmapper class 로 변경하기 > builder 로 하니까 mapping이 제대로 되지 않나봐..
+    public Art createArt() {
+        List<Image> imageList = new ArrayList<>();
+        Art art = Art.builder()
+                .name(this.name)
+                .series(this.series)
+                .description(this.description)
+                .widthSize(this.widthSize)
+                .heightSize(this.heightSize)
+                .materialStatus(this.materialStatus)
+                .artStatus(this.artStatus)
+                .notice(this.notice)
+                .cherryNum(this.cherryNum)
+                .build();
 
-    public Art createArt(){
-        return modelMapper.map(this, Art.class);
+
+        if (this.getImageList() != null && !this.getImageList().isEmpty()) {
+            imageList = this.getImageList().stream()
+                    .map(image -> {
+                        ImageCreateRequestDto imageCreateRequestDto = new ImageCreateRequestDto();
+                        return imageCreateRequestDto.createImage();
+                    }).toList();
+            art.addImageList(imageList);
+        }
+
+        return art;
     }
 
-    public static ArtCreateRequestDto of(Art art){
-        return modelMapper.map(art,ArtCreateRequestDto.class);
+    public ArtCreateRequestDto of(Art art) {
+
+        List<ImageCreateRequestDto> imageCreateRequestDtoList = art.getImageList().stream()
+                .map(image -> {
+                    ImageCreateRequestDto imageCreateRequestDto = new ImageCreateRequestDto();
+                    return imageCreateRequestDto.of(image);
+                }).toList();
+
+        this.artStatus = art.getArtStatus();
+        this.name = art.getName();
+        this.series = art.getSeries();
+        this.description = art.getDescription();
+        this.widthSize = art.getWidthSize();
+        this.heightSize = art.getHeightSize();
+        this.materialStatus = art.getMaterialStatus();
+        this.notice = art.getNotice();
+        this.cherryNum = art.getCherryNum();
+        this.imageList = imageCreateRequestDtoList;
+        return this;
     }
 }
