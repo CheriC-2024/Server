1a76a1fcb729928e75754f4e7e0b7990f2c0fdb9
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-06-11 18:55:55 +0900

    ✨ Feat: Add art part enum class (#3)

---
+++ src/main/java/com/art/cheric/constant/PartSort.java
@@ -0,0 +1,11 @@
+package com.art.cheric.constant;
+
+public enum PartSort {
+    PAINTING,
+    OIL_PAINTING,
+    WATER_PAINTING,
+    WESTERN_PAINTING,
+    ORIENTAL_PAINTING,
+    DIGITAL_ART,
+    POP_ART,
+}
