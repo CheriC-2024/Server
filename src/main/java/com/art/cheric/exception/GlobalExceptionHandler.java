f5ce4a4242259a615f7011698a30a953d394cf81
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-06-12 18:49:33 +0900

    🔥 Remove: Delete outOfBoundException from globalExceptionHandler (#4)

--- src/main/java/com/art/cheric/exception/GlobalExceptionHandler.java
+++ src/main/java/com/art/cheric/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -12,12 +11,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex) {
-        return ResponseEntity.badRequest().body("유효성 검사 실패: " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("유효성 검사 실패: " + ex.getMessage());
     }
 
-    @ExceptionHandler(CherryOutOfRangeException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<String> handleCherryOutOfRangeException(CherryOutOfRangeException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-    }
 }
