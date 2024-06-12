244d8246c49e59daebd0cd86faf5c9c983700799
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-06-12 18:53:54 +0900

    ✨ Feat: Add findListCollection method in CollectionRepository (#4)

--- src/main/java/com/art/cheric/repository/CollectionRepository.java
+++ src/main/java/com/art/cheric/repository/CollectionRepository.java
@@ -3,6 +3,9 @@ package com.art.cheric.repository;
 import com.art.cheric.entity.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CollectionRepository extends JpaRepository<Collection, Long> {
 
+    List<Collection> findByIdIn(List<Long> ids);
 }
