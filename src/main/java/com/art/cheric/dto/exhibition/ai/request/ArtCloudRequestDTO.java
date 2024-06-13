a89cae60fbd4247830b20ff8fdf010ab8e7eab38
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-06-13 16:07:01 +0900

    ✨Feat : Add cloud request type in ArtCloudRequestDTO (#6)

--- src/main/java/com/art/cheric/dto/exhibition/ai/request/ArtCloudRequestDTO.java
+++ src/main/java/com/art/cheric/dto/exhibition/ai/request/ArtCloudRequestDTO.java
@@ -1,13 +1,21 @@
 package com.art.cheric.dto.exhibition.ai.request;
 
 
+import com.art.cheric.constant.CloudRequestType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
 public class ArtCloudRequestDTO {
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private CloudRequestType cloudRequestType;
+
     @NotEmpty
     private List<Long> artIds;
 
