124531ae7a4dc1c5f5f76dc9e4e515d1844195a0
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-06-14 05:36:37 +0900

    🔨 Fix : Add filepath for collectionReadAllDto (#7)

--- src/main/java/com/art/cheric/dto/collection/respond/CollectionReadAllDto.java
+++ src/main/java/com/art/cheric/dto/collection/respond/CollectionReadAllDto.java
@@ -8,11 +8,15 @@ public class CollectionReadAllDto {
     private Long id;
     private String name;
     private String description;
+    private String filePath;
 
     public CollectionReadAllDto of(Collection collection) {
         this.id = collection.getId();
         this.description = collection.getDescription();
         this.name = collection.getName();
+        if(!collection.getCollectionArtList().isEmpty()){
+            this.filePath = collection.getCollectionArtList().get(0).getArt().getImageList().get(0).getFilePath();
+        }
         return this;
     }
 }
