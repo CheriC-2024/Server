dd164cca63667b54faf6c55983dfba8f7a09bd63
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-06-18 16:05:05 +0900

    ✨ Feat : Add musicgen response, request dto (#15)

---
+++ src/main/java/com/art/cheric/dto/exhibition/ai/request/MusicZenRequestDTO.java
@@ -0,0 +1,10 @@
+package com.art.cheric.dto.exhibition.ai.request;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class MusicZenRequestDTO {
+    private List<String> theme;
+}
