1bd77059252bf92a47cbf82f7c76b3693dc821c6
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-06-18 14:28:14 +0900

    🔨 Fix : Add default creator in ArtCloudResponseDTO (#10)

--- src/main/java/com/art/cheric/dto/exhibition/ai/respond/ArtCloudResponseDTO.java
+++ src/main/java/com/art/cheric/dto/exhibition/ai/respond/ArtCloudResponseDTO.java
@@ -10,6 +10,9 @@ public class ArtCloudResponseDTO {
     private Long artId;
     private List<String> properties;
 
+    public ArtCloudResponseDTO () {
+    }
+
     public ArtCloudResponseDTO (Long artId) {
         this.artId = artId;
         this.properties = new ArrayList<>();
